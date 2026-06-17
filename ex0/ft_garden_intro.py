ea40d93504d8f9123a3660b9a50ac464a6e50e91
ytrabels <ytrabels@f1r9s12.paris.42.school>
2026-06-17 16:50:57 +0200

    ex0 done

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    name: str = "Rose"
+    height: float = 25
+    age: int = 30
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}\nHeight: {height}cm\nAge: {age} days\n")
+    print("=== End of Program ===")
