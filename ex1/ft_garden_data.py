7b8ff3c38a2d5b06321ba5c7eabcaaece3889545
YanisTrabelsi <yanis.trabelsi@epitech.digital>
2026-06-18 14:53:02 +0200

    ex1

---
+++ ex1/ft_garden_data.py
@@ -0,0 +1,10 @@
+class Plant:
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show():
+        name_upper = self.name.capit 
+         
+
