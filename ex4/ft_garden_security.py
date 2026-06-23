6371af8ef128e75ef302198ecbda6e4612010c86
ytrabels <ytrabels@f0r12s3.paris.42.school>
2026-06-23 18:38:17 +0200

    error in red

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -7,9 +7,10 @@ class Plant:
         self.growth_rate: float = growth_rate if growth_rate > 0 else 0.01
 
         if(height < 0):
-            print("Invalid negative height !\nHeight set to default value")
+            print("\033[31mInvalid negative height !\nHeight set to default value\33[0m")
         if(days < 0):
-            print("Invalid negative age !\nAge set to default value")
+            print("\33[31mInvalid negative age !\nAge set to default value\033[0m")
+        print("Created: ", end="")
         self.show()
 
     def show(self) -> None:
@@ -24,15 +25,15 @@ class Plant:
 
     def set_height(self, value: float) -> None:
         if (value < 0):
-            print(f"{self.name}: Error, height can’t be negative")
-            return (print("Height update rejected\n"))
+            print(f"\033[31m{self.name}: Error, height can’t be negative")
+            return (print("Height update rejected\n\33[0m"))
         self._height = value
         print(f"Height updated: {value}cm")
 
     def set_age(self, value: int) -> None:
         if (value < 0):
-            print(f"{self.name}: Error, age can’t be negative")
-            return (print("Age update rejected\n"))
+            print(f"\033[31m{self.name}: Error, age can’t be negative")
+            return (print("Age update rejected\n\033[0m"))
         self._days = value
         print(f"Age updated: {value} days")
 
@@ -44,7 +45,8 @@ class Plant:
 
 
 print("=== Garden Security System ===")
-rose: Plant = Plant("rose", 25, 30, 0.8)
-rose.set_age(-5)
+rose: Plant = Plant("rose", -25, 30, 0.8)
+rose.set_age(5)
+rose.set_height(-2)
 print("Current state: ", end="")
 rose.show()
